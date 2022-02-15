6e2b28661942aee6f4c1b80d861b436655ceded3
Marina Fomina <demi87@mail.ru>
2022-02-16 01:15:03 +0700

    add gitignore,build.gradle,CashbackHackService

---
+++ src/main/java/ru/netology/service/CashbackHackService.java
@@ -0,0 +1,8 @@
+package ru.netology.service;
+
+public class CashbackHackService {
+    public int remain(int amount) {
+        int boundary = 1000;
+        return boundary - amount % boundary;
+    }
+}
